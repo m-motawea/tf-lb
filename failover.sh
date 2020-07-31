90f68e7004d7cee8040b3235a9eb120b3132b133
Maged Motawea <magedmotawea@gmail.com>
2020-07-31 19:46:11 +0200

    never kill trc on master

--- failover.sh
+++ failover.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 if ["$3" == "MASTER"]; then 
+    if pgrep -x "trc" > /dev/null; then
+        echo "trc is running"
+    else
+        /trc -local 127.0.0.1:80 -local-tls 127.0.0.1:443 -remote $TFGATEWAY &
+    fi
+else 
     pkill -9 trc
-    /trc -local 127.0.0.1:80 -local-tls 127.0.0.1:443 -remote $TFGATEWAY &
+fi
